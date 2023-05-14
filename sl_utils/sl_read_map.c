39a1b3d741dc13f8c5f45daa6644c81c7ccb18bb
tmoumni <tmoumni@student.42.fr>
2023-05-14 15:27:57 +0100

    tmoumni

--- sl_utils/sl_read_map.c
+++ sl_utils/sl_read_map.c
@@ -6,7 +6,7 @@
 /*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 15:26:39 by tmoumni           #+#    #+#             */
-/*   Updated: 2023/05/14 15:11:34 by tmoumni          ###   ########.fr       */
+/*   Updated: 2023/05/14 15:17:46 by tmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,7 +79,7 @@ void	read_map(char *av, t_game *game)
 	while (line)
 	{
 		if (i == game->height - 1 && has_nl(line))
-			exit (1);
+			exit (0);
 		remove_nl(line);
 		game->map[i] = line;
 		line = get_next_line(fd);
--- sl_utils/sl_win_game.c
+++ sl_utils/sl_win_game.c
@@ -6,7 +6,7 @@
 /*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:53:52 by tmoumni           #+#    #+#             */
-/*   Updated: 2023/05/14 10:21:49 by tmoumni          ###   ########.fr       */
+/*   Updated: 2023/05/14 15:14:12 by tmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	win_game(t_game *g, int i, int j)
 {
 	if (g->map[i][j] == 'E' && g->clcted == g->clctbls)
 	{
+		g->mvmnts++;
 		ft_printf("\n\033[1;32m[[ Congratulations ]]\033[0m\n");
 		ft_printf("\033[1;32m[[ U win in %d moves ]]\033[0m\n", g->mvmnts);
 		ft_printf("\033[1;32m[[ Is that the best u can do ? ]]\033[0m\n\n");
